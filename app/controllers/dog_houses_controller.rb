90fa0b694891201232de2f5ddfb6c1f0aac96ab1
felixNyalenda <felixasejunior@gmail.com>
2023-06-22 19:45:55 +0300

    complete assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,12 +1,21 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def show
-    dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+  def index
+    dog_houses = DogHouse.all
+    render json: dog_houses, include: {reviews: {except: [:created_at, :updated_at, ]} }, except: [:created_at, :updated_at], status: :ok
   end
+  def show
+    dog_house = find_dog_house
+    render json: dog_house, include: {reviews: {except: [:created_at, :updated_at]} }, except: [:created_at, :updated_at], status: :ok
+  end
+  
 
   private
+
+  def find_dog_house
+    DogHouse.find(params[:id])
+  end
 
   def render_not_found_response
     render json: { error: "Dog house not found" }, status: :not_found
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,40 @@
 class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  wrap_parameters format: []
+
   def index
-    reviews = Review.all
-    render json: reviews, include: :dog_house
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+    render json: reviews, include: {dog_house: {except: [:updated_at, :created_at]} }, except: [:created_at, :updated_at, :dog_house_id], status: :ok
   end
 
   def show
-    review = Review.find(params[:id])
-    render json: review, include: :dog_house
+    review = find_review
+    render json: review, include: {dog_house: {except: [:updated_at, :created_at]} }, except: [:created_at, :updated_at, :dog_house_id], status: :ok
   end
 
   def create
     review = Review.create(review_params)
-    render json: review, status: :created
+    render json: review, except: [:created_at, :updated_at, :dog_house_id], status: :created
   end
 
   private
+
+  def find_review
+    Review.find(params[:id])
+  end
 
   def render_not_found_response
     render json: { error: "Review not found" }, status: :not_found
   end
 
   def review_params
-    params.permit(:username, :comment, :rating)
+    params.permit(:user, :comment, :rating)
   end
 
 end
